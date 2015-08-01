df250b8e8a6c0e997a9f1485976d6234ca9c19fd
Michael Jalkio <mjalkio@gmail.com>
2015-08-01 12:22:44 +0300

    Add settings.rb and saved_trainers.marshal

---
+++ settings.rb
@@ -0,0 +1,13 @@
+module Settings
+    attr_accessor :client_id
+    attr_accessor :client_secret
+    attr_accessor :username
+    attr_accessor :user_password
+    attr_accessor :app_id
+
+    @client_id = '***REMOVED***'
+    @client_secret = '***REMOVED***'
+    @username = '***REMOVED***'
+    @user_password = '***REMOVED***'
+    @app_id = ***REMOVED***
+end
