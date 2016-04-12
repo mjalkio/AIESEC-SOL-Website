c798c2363f8bd8bf5f400c69f56f9ea718e2b4bf
Michael Jalkio <mjalkio@gmail.com>
2016-04-12 16:21:02 +0300

    Removed sensitive data

--- settings.rb
+++ settings.rb
@@ -5,9 +5,9 @@ module Settings
     attr_accessor :user_password
     attr_accessor :app_id
 
-    @client_id = '***REMOVED***'
-    @client_secret = '***REMOVED***'
-    @username = '***REMOVED***'
-    @user_password = '***REMOVED***'
-    @app_id = ***REMOVED***
+    @client_id = ''
+    @client_secret = ''
+    @username = ''
+    @user_password = ''
+    @app_id = 1234567
 end
